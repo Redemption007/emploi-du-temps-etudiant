d81470aa0582c5e0cf4c2cc629964a9c0fcd4096
Redemption07 <b.a.p.t.s.t.e7@gmail.com>
2022-09-09 01:52:49 +0200

    Organisation des pages Création des boutons du menu

--- App.js
+++ App.js
@@ -1,12 +1,63 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
+
+  const Menu = ({ navigation }) => {
+    return (
+      <View style={styles.menu}>
+        <Button
+          title="Accueil"
+          onPress={() => navigation.navigate('Resume')}
+        />
+        <Button
+          title='Tâches'
+          onPress={() => navigation.navigate('Tasks')}
+        />
+        <Button
+          title='Planning'
+          onPress={() => navigation.navigate('Planning')}
+        />
+      </View>
+    );
+  }
+  const Resume = ({ navigation }) => {
+    return (
+      <Menu navigation={navigation}/>
+    );
+  };
+  const Tasks = ({ navigation }) => {
+    return (
+      <Menu navigation={navigation}/>
+    );
+  }
+  const Planning = ({ navigation }) => {
+    return (
+      <Menu navigation={navigation}/>
+    );
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Resume"
+          component={Resume}
+        />
+        <Stack.Screen
+          name="Tasks"
+          component={Tasks}
+        />
+        <Stack.Screen
+          name="Planning"
+          component={Planning}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -17,4 +68,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  menu: {
+    flex: 1,
+    backgroundColor: '#000',
+    alignItems: 'flex-end',
+    justifyContent: 'space-between'
+  }
 });
